8789b465660ef236ab9a6288a943c404bb574e62
TeamB-lab <teamb.teamproject@gmail.com>
2025-12-11 11:55:07 +0100

    Refactor router imports and route definitions

--- src/router/index.js
+++ src/router/index.js
@@ -1,45 +1,49 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-
-import HomePage from '../views/HomePage.vue'
-import CheckoutPage from '../views/CheckoutPage.vue'
-import ThankYouPage from '../views/ThankYouPage.vue'
-import HomePage from '../components/HomePage.vue'  
+// COMPONENTS
+import HomePage from '../components/HomePage.vue'
 import Register from '../components/Register.vue'
 import Products from '../components/Products.vue'
+
+// VIEWS
+import CheckoutPage from '../views/CheckoutPage.vue'
+import ThankYouPage from '../views/ThankYouPage.vue'
+
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: HomePage
+    component: HomePage,
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register,
+  },
+  {
+    path: '/products',
+    name: 'Products',
+    component: Products,
   },
   {
     path: '/checkout',
     name: 'Checkout',
-    component: CheckoutPage
+    component: CheckoutPage,
   },
   {
     path: '/thank-you',
     name: 'ThankYou',
-    component: ThankYouPage
-    path: '/register',
-    name: 'Register', 
-    component: Register
+    component: ThankYouPage,
   },
-
-  {path :'/products',
-  name :'Products',
-  component:Products
-  }
 ]
+
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
   routes,
-  scrollBehavior(to,from,savedPosition){
-  return {top:0}
+  scrollBehavior(to, from, savedPosition) {
+    return { top: 0 }
   }
 })
 
